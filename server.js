35548baed240b67fb53b09c6996564f4f3d44a19
zzacong <ongzz100@gmail.com>
2020-10-19 12:44:40 +1100

    use nodemon

--- server.js
+++ server.js
@@ -62,4 +62,7 @@ app.post('/purchase', (req, res) => {
   })
 })
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
