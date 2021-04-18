087aeb01e9b9550027f7f5877eb00b3915d902af
zzacong <ongzz100@gmail.com>
2021-04-18 14:21:24 +1000

    update rename

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
-console.log(stripeSecretKey, stripePublicKey)
+// console.log(stripeSecretKey, stripePublicKey)
 
 const express = require('express')
 const app = express()
